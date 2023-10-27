d03d45099ef079b4d0c259d936382bcee23abd40
Nawfel Sekrafi <sekrafinawfel@gmail.com>
2023-10-27 08:00:58 +0100

    Initial Commit

---
+++ ios-course-assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-course-assignment
+//
+//  Created by Nawfel Sekrafi on 27/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
